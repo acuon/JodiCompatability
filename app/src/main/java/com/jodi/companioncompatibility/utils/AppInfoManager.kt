4c84003c2147203d43169c65cff37b08bb034d0b
Rohit Sharma <rohit@flhealth.in>
2024-02-03 04:38:17 +0530

    feat(utils): :sparkles: add gpt chat completion endpoint

--- app/src/main/java/com/jodi/companioncompatibility/utils/AppInfoManager.kt
+++ app/src/main/java/com/jodi/companioncompatibility/utils/AppInfoManager.kt
@@ -1,6 +1,11 @@
 package com.jodi.companioncompatibility.utils
 
 object AppInfoManager {
+
+    object GPT {
+        const val CHAT_COMPLETIONS = "v1/chat/completions"
+    }
+
     object CACHE {
     }
 }
